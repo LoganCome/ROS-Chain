5dcd83978227077e21dca77401573a725f08c67a
o0Freak0o <1918192086@qq.com>
2021-08-19 17:25:57 +0800

    new features

--- udp_receiver/Hear_Node.py
+++ udp_receiver/Hear_Node.py
@@ -1,47 +1,63 @@
-from socket import *
+# -*- coding = utf-8 -*-
+# @Time:2021-08-19 16:50
+# @Author:Freak
+# @File:dxada.py
+# @SoftWare:PyCharm
+import time
 from web3 import Web3
 import json
+from redis_flush import data_obtain
 
+"""
+基于以太坊的无人车只能调度模型
+上链操作管理模块
+
+本项目由海南大学机器人与人工智能协会区块链小组成员共同维护
+"""
+
+#根据约定设置机器人名称
+Robot_1_name = "Robot_1"
+Robot_2_name = "Robot_2"
+#定义字典，方便以太坊账号操作以及链码初始化
+Robot = {Robot_1_name : 0,Robot_2_name : 1}
+
+#根据本地链码的二进制文件以及以太坊上的链码部署地址初始化合约
 def load_via_artifact(w3, contract):
+    #二进制文件路径
     fn_abi = './{0}.abi'.format(contract)
+    #地址文件路径
     fn_addr = './{0}.addr'.format(contract)
-
+    #读取二进制文件
     with open(fn_abi) as f:
         abi = json.load(f)
-
+    #读取地址文件
     with open(fn_addr) as f:
         addr = f.read()
-
+    #初始化合约
     return w3.eth.contract(address=addr, abi=abi)
 
-def Recv():
-    Udp_Socket = socket(AF_INET,SOCK_DGRAM)
-    Udp_Socket.bind(("192.168.1.102",8848))
+#上链操作管理
+def data_on_chain(name):
+    #初始化web3对象
     w3 = Web3(Web3.HTTPProvider('http://localhost:8545'))
+    #加载合约账户
     accounts = w3.eth.accounts
+    #加载合约
     token = load_via_artifact(w3, 'Robotchain')
-    token.functions.registerRobot(1).transact({'from': accounts[0]})
-    while 1:
-        Recv_Data = Udp_Socket.recvfrom(1024)
-        # Recv_Msg = Recv_Data[0]
-        # Sender_Addr = Recv_Data[1]
-        # print("%s:%s"%(str(Sender_Addr),Recv_Msg.decode('utf-8')))
-        Recv_msg = Recv_Data[0].decode('utf-8')
-        Recv_msg = Recv_msg.split("+")
+    #调用链上函数，在区块链上注册机器人
+    token.functions.registerRobot(Robot[name]+1).transact({'from': accounts[Robot[name]]})
+    #从redis中获取数据
+    while True:
+        #获取数据
+        x,y = data_obtain(name)
+        #调用链上方法，设置机器人位置信息
+        token.functions.setPosition(x,y).transact({'from': accounts[Robot[name]]})
+        #获取链上机器人位置
+        (tx, ty) = token.functions.getOwnPosition().call({'from': accounts[0]})
+        cur_time = time.time()
+        #打印
+        print("Current_Time: "+cur_time+", and the cur position is :"+tx+", "+ty)
 
-        tar_x = int(Recv_msg[0])
-        tar_y = int(Recv_msg[1])
-        print(tar_x)
-        print(tar_y)
-
-        token.functions.setPosition(tar_x,tar_y).transact({'from': accounts[0]})
-        token.functions.setVel(0).transact({'from': accounts[0]})
-
-        (tx,ty) = token.functions.getOwnPosition().call({'from': accounts[0]})
-        print(tx)
-        print(ty)
-        # print (Recv_msg)
-        # while 1:
-        #     print(Recv_msg)
 if  __name__ == "__main__":
-    Recv()
+    data_on_chain(Robot_1_name)
+    data_on_chain(Robot_2_name)
---
+++ udp_receiver/redis_flush.py
@@ -0,0 +1,138 @@
+# -*- coding = utf-8 -*-
+# @Time:2021-08-19 16:14
+# @Author:Freak
+# @File:flush.py
+# @SoftWare:PyCharm
+"""
+基于以太坊的无人车只能调度模型
+Redis缓存
+说明：由于ROS pub信息的速率过高，而强制降低消息发送速率会影响底盘信息调制。
+从而影响建图、导航等相关功能
+因此做一层缓存，将数据存储到redis中去，并且手动进行去重存储
+这样不仅能规避消息发送速率过高的负面影响，而且可以保证区块上记录的信息为最新的信息，有效降低区块链负载
+
+本项目由海南大学机器人与人工智能协会区块链小组成员共同维护
+"""
+import redis
+from socket import *
+import threading
+import signal
+
+'''线程池子，初始化为空'''
+threads = []
+'''线程开关控制变量'''
+thread_on = True
+'''本机IP，用作Redis连接以及端口监听'''
+host = 'localhost'
+'''Redis连接端口号'''
+port = 6379
+'''机器人1信息监听端口'''
+Robot_1_port = 8848
+'''机器人2信息监听端口'''
+Robot_2_port = 8849
+'''机器人1名字'''
+Robot_1_name = "Robot_1"
+'''机器人2名字'''
+Robot_2_name = "Robot_2"
+
+'''按下ctrl+c的回调函数'''
+def sigint_handler(signum, frame):
+    print("The thread of receiving data will be killed!")
+    '''将全局的线程开关控制变量设False，即关闭线程'''
+    global thread_on
+    thread_on = False
+    print("The killing is going on.")
+
+'''Redis连接函数'''
+def Connect_redis():
+    pool = redis.ConnectionPool(host=host, port=port, deploy_contract=True)
+    r = redis.Redis(host=host, port=port, decode_responses=True)
+    print('Connection established!')
+
+'''机器人1端口监听'''
+def Recv_data_from_Robot_1():
+    '''创建socket'''
+    Udp_Socket = socket(AF_INET, SOCK_DGRAM)
+    '''绑定端口'''
+    Udp_Socket.bind(("192.168.1.107", Robot_1_port))
+    '''线程执行任务'''
+    while thread_on:
+        '''接受端口发送的数据'''
+        Recv_Data = Udp_Socket.recvfrom(1024)
+        '''数据解码'''
+        Recv_msg = Recv_Data[0].decode('utf-8')
+        '''数据分割处理'''
+        Recv_msg = Recv_msg.split("+")
+
+        tar_x = int(Recv_msg[0])
+        tar_y = int(Recv_msg[1])
+        print("Data Receive from Robot_1! Now ready to process.")
+        '''处理数据'''
+        data_process(tar_x, tar_y, Robot_1_name)
+
+'''机器人2端口监听'''
+def Recv_data_from_Robot_2():
+    Udp_Socket = socket(AF_INET, SOCK_DGRAM)
+    Udp_Socket.bind(("192.168.1.107", Robot_2_port))
+    while 1:
+        Recv_Data = Udp_Socket.recvfrom(1024)
+        Recv_msg = Recv_Data[0].decode('utf-8')
+        Recv_msg = Recv_msg.split("+")
+
+        tar_x = int(Recv_msg[0])
+        tar_y = int(Recv_msg[1])
+        print("Data Receive from Robot_2! Now ready to process.")
+        data_process(tar_x, tar_y, Robot_2_name)
+
+'''数据处理函数'''
+def data_process(x, y, name):
+    '''建立redis连接'''
+    r = redis.Redis(host=host, port=port, decode_responses=True)
+    '''list初始化'''
+    r.rpushx(name + "_x", 0)
+    r.rpushx(name + "_y", 0)
+    temp_x = r.brpop(name + "_x")
+    temp_y = r.brpop(name + "_y")
+    '''比较此时拿到的数据和list中上一个数据新型比对，若相同则不入库，否则append到末尾'''
+    if x == temp_x and y == temp_y:
+        r.rpushx(name + "_x", temp_x)
+        r.rpushx(name + "_y", temp_y)
+    else:
+        r.rpushx(name + "_x", temp_x)
+        r.rpushx(name + "_y", temp_y)
+        r.rpushx(name + "_x", x)
+        r.rpushx(name + "_y", y)
+
+'''数据获取函数'''
+def data_obtain(name):
+    '''建立连接'''
+    r = redis.Redis(host=host, port=port, decode_responses=True)
+    '''获取List的长度，以get到list中最后一个信息'''
+    length = r.llen(name + "_x")
+    '''获取list中最新的信息'''
+    x = r.lindex(name + "_x", length - 1)
+    y = r.lindex(name + "_y", length - 1)
+    return x,y
+
+'''主函数'''
+if __name__ == '__main__':
+    '''Redis建立连接'''
+    Connect_redis()
+
+    '''创建两条线程，分别用于监听两台车的数据'''
+    t1 = threading.Thread(target=Recv_data_from_Robot_1)
+    t2 = threading.Thread(target=Recv_data_from_Robot_2)
+    '''开启线程'''
+    t1.start()
+    t2.start()
+    '''线程加入线程池'''
+    threads.append(t1)
+    threads.append(t2)
+    '''信号接收'''
+    signal.signal(signal.SIGINT, sigint_handler)
+    '''阻塞子线程线程'''
+    for t in threads:
+        t.join()
+    '''子线程执行完毕，回到主线程执行一下语句后，程序执行完毕'''
+    print("Flush closed!")
+
